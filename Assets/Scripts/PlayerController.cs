547c7f1c5ebdf4462ab2b7a24ca27a2ba0d1159b
“Sahin” <sahinhalil1311@gmail.com>
2025-10-17 23:45:41 +0100

    Added simple mouse movement for player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,27 +7,38 @@ public class PlayerController : MonoBehaviour
 {
     
     private CharacterController characterController;
+    
     [SerializeField] private float speed;
     [SerializeField] private Vector2 move;
 
+    [SerializeField] private float mouseSense;
+    [SerializeField] private Vector2 mouse;
+    [SerializeField] private float lookHorizontal = 0;
+    [SerializeField] private float lookVertical = 0;
+
+    private void OnMove(InputValue value) {
+        move = value.Get<Vector2>();
+    }
+    private void OnLook(InputValue value) { 
+        Debug.Log("here is the coords += " + value.Get<Vector2>());
+        mouse = value.Get<Vector2>();
+        lookHorizontal -= mouse.y * mouseSense;
+        lookVertical += mouse.x * mouseSense;
+    }
+
+    // Awake is called before the first frame update
     void Awake()
     {
         characterController = GetComponent<CharacterController>();
         speed = 5f;
-    }
-    private void OnMove(InputValue value) {
-        move = value.Get<Vector2>();
-    }
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
+        mouseSense = 0.5f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        characterController.SimpleMove(new Vector3(move.x, 0, move.y) * speed);        
+        characterController.SimpleMove(new Vector3(move.x, 0, move.y) * speed);
+        //transform.Rotate(new Vector3(0, mouse.x, -mouse.y) * mouseSense * Time.deltaTime, Space.World);
+        transform.rotation = Quaternion.Euler(lookHorizontal, lookVertical, 0);
     }
 }
