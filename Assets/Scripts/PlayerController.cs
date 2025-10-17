e083137ab10b15120c9a59c32ec8b064d6b4e0ab
“Sahin” <sahinhalil1311@gmail.com>
2025-10-18 00:09:57 +0100

    Added comments for controls script and clamped and centered cursor

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,40 +5,46 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    // Components
     private CharacterController characterController;
-    
+
+    // Movement
     [SerializeField] private float speed;
     [SerializeField] private Vector2 move;
 
+    // Mouse look
     [SerializeField] private float mouseSense;
     [SerializeField] private Vector2 mouse;
     [SerializeField] private float lookHorizontal = 0;
     [SerializeField] private float lookVertical = 0;
 
-    private void OnMove(InputValue value) {
+    // Called when movement input is detected
+    private void OnMove(InputValue value)
+    {
         move = value.Get<Vector2>();
     }
-    private void OnLook(InputValue value) { 
-        Debug.Log("here is the coords += " + value.Get<Vector2>());
+
+    // Called when mouse input is detected
+    private void OnLook(InputValue value)
+    {
         mouse = value.Get<Vector2>();
-        lookHorizontal -= mouse.y * mouseSense;
-        lookVertical += mouse.x * mouseSense;
+        lookHorizontal = Mathf.Clamp(lookHorizontal - (mouse.y * mouseSense), -90f, 90f);
+        lookVertical = Mathf.Clamp(lookVertical + (mouse.x * mouseSense), -90f, 90f);
     }
 
-    // Awake is called before the first frame update
+    // Runs before Start, used for setup
     void Awake()
     {
         characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
         speed = 5f;
         mouseSense = 0.5f;
     }
 
-    // Update is called once per frame
+    // Called every frame
     void Update()
     {
         characterController.SimpleMove(new Vector3(move.x, 0, move.y) * speed);
-        //transform.Rotate(new Vector3(0, mouse.x, -mouse.y) * mouseSense * Time.deltaTime, Space.World);
         transform.rotation = Quaternion.Euler(lookHorizontal, lookVertical, 0);
     }
 }
