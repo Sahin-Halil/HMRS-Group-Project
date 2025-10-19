1868d61924ffb4af61a46bb6d13729997ecc0cf9
“Sahin” <sahinhalil1311@gmail.com>
2025-10-20 00:33:06 +0100

    Normalised move vector for diagonal movement and tidied up PlayerController Script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,17 +9,18 @@ public class PlayerController : MonoBehaviour
     private CharacterController characterController;
 
     // Movement
-    [SerializeField] private float standingSpeed;
+    [SerializeField] private float standingSpeed = 5f;
     [SerializeField] private Vector3 move;
     [SerializeField] private float xMove;
     [SerializeField] private float yMove;
 
     // Mouse look
-    [SerializeField] private float mouseSense;
+    [SerializeField] private float mouseSense = 0.5f;
     [SerializeField] private float xRotation;
     [SerializeField] private float yRotation;
 
-    private bool crouchInput;
+    // Crouch
+    [SerializeField] private bool crouchInput = false;
     private float crouchSpeed;
     private float crouchHeight;
 
@@ -29,47 +30,41 @@ public class PlayerController : MonoBehaviour
         Vector2 moveInput = value.Get<Vector2>();
         xMove = moveInput.x;
         yMove = moveInput.y;
-        //Debug.Log((xMove, yMove));
     }
 
     // Called when mouse input is detected
     private void OnLook(InputValue value)
     {
         Vector2 mouseInput = value.Get<Vector2>();
-        //Debug.Log(mouse);
-        //Debug.Log(direction);
         xRotation = xRotation + (mouseInput.x * mouseSense);
         yRotation = Mathf.Clamp(yRotation - (mouseInput.y * mouseSense), -90f, 90f);
     }
 
-    private void OnCrouch() {
-        Debug.Log("hello");
+    // Handles crouch toggling
+    private void OnCrouch()
+    {
         crouchInput = !crouchInput;
         characterController.height -= crouchHeight;
         crouchHeight *= -1f;
     }
 
-    // Runs when game starts, used for setup
+    // Setup components and values
     void Awake()
     {
         characterController = GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked;
-        standingSpeed = 5f;
-        xMove = 0f;
-        yMove = 0f;
-        mouseSense = 0.5f;
-        xRotation = 0f;
-        yRotation = 0f;
-        crouchInput = false;
         crouchSpeed = 0.5f * standingSpeed;
         crouchHeight = 0.25f * characterController.height;
     }
 
-    // Called every frame
+    // Handles movement and rotation each frame
     void Update()
     {
         float currentSpeed = crouchInput ? crouchSpeed : standingSpeed;
         move = transform.right * xMove + transform.forward * yMove;
+        if (move.magnitude > 1) { 
+            move.Normalize();
+        }
         characterController.SimpleMove(move * currentSpeed);
         transform.rotation = Quaternion.Euler(yRotation, xRotation, 0f);
     }
