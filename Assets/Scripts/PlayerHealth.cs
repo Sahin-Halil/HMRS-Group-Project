fb5ff2807e6c657f55a1421b3a47c4fa768dc976
nzssmph <h.tang@se23.qmul.ac.uk>
2025-10-20 13:56:30 +0100

    add health-ui

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float maxHealth = 100f;
+    public float currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    void Update()
+    {
+        // Simulate health decreasing over time
+        currentHealth -= Time.deltaTime * 5; // Decrease health over time
+
+        if (currentHealth <= 0)
+        {
+            Debug.Log("Player is dead!");
+        }
+    }
+}
+
+
